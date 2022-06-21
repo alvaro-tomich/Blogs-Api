cafa8898053ba57d06e7cafab899c7e8da6e7f5b
alvaro-tomich <alvaroramos222@hotmail.com>
2022-06-21 13:57:31 -0300

    feat(migrations): created BlogPosts table

---
+++ src/database/migrations/20220621164327-create-blog-posts.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING(255)
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING(255)
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        foreignKe: true
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('BlogPosts')
+  }
+};
