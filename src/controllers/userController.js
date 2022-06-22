6d4aaeabce795cc210826b69c15c924d8cf7cb6c
alvaro-tomich <alvaroramos222@hotmail.com>
2022-06-22 16:02:36 -0300

    feat(controller/user): created getById function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,4 +16,15 @@ const getUsers = async (_req, res) => {
     res.status(200).json(users);
 };
 
-module.exports = { createUser, getUsers };
+const getById = async (req, res) => {
+    const { id } = req.params;
+    const user = await userService.getById(id);
+
+    if (!user) {
+        return res.status(404).json({ message: 'User does not exist' });
+    }
+
+    res.status(200).json(user);
+};
+
+module.exports = { createUser, getUsers, getById };
