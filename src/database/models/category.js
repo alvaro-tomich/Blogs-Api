8260c6bbb251291b44b53a01f9879b6773f46813
alvaro-tomich <alvaroramos222@hotmail.com>
2022-06-22 16:32:29 -0300

    refactor(models/category): add timestamps false condition

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -2,7 +2,7 @@ const categorySchema = (sequelize, dataTypes) => {
     const categoryTable = sequelize.define('Category', {
         id: { type: dataTypes.INTEGER, primaryKey: true },
         name: dataTypes.STRING(255)
-    })
+    }, { timestamps: false })
 
     return categoryTable;
 }
