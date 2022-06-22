6f09e6cfb6abf9ee518b0f112503d9d30bfe26f3
alvaro-tomich <alvaroramos222@hotmail.com>
2022-06-22 12:54:44 -0300

    requirement 5 complete

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const userMiddleware = require('../middlewares/userMiddleware');
+const tokenMiddleware = require('../middlewares/tokenMiddleware');
 
 const route = express.Router();
 
 route.post('/user', userMiddleware, userController.createUser);
+route.get('/user', tokenMiddleware, userController.getUsers);
 
 module.exports = route;
