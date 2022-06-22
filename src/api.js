ad3c9c315a66790597aba9c7c06964e9e08ee633
alvaro-tomich <alvaroramos222@hotmail.com>
2022-06-22 10:03:00 -0300

    requirement 3 complete

--- src/api.js
+++ src/api.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const authRoute = require('./routes/authRoute');
 
 // ...
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/', authRoute);
 
 // ...
 
