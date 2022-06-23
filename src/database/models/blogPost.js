1be7d6b7a7e49fb621959f90b6559c851828512b
alvaro-tomich <alvaroramos222@hotmail.com>
2022-06-23 17:26:19 -0300

    refactor(models/blogPost): added autoIncrement and timestamps

--- src/database/models/blogPost.js
+++ src/database/models/blogPost.js
@@ -1,12 +1,12 @@
 const blogPostSchema = (sequelize, dataTypes) => {
     const blogsPostTable = sequelize.define('BlogPost', {
-        id: { type: dataTypes.INTEGER, primaryKey: true },
+        id: { type: dataTypes.INTEGER, autoIncrement: true, primaryKey: true },
         title: dataTypes.STRING(255),
         content: dataTypes.STRING(255),
         userId: { type: dataTypes.INTEGER, foreignKey: true },
         published: dataTypes.DATE,
         updated: dataTypes.DATE
-    })
+    }, {createdAt: 'published', updatedAt: 'updated'})
 
     blogsPostTable.associate = (models) => {
         blogsPostTable.belongsTo(models.User, 
