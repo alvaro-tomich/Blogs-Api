372ee77200fc578cdfefadf26f4a530f2e2aa6c1
alvaro-tomich <alvaroramos222@hotmail.com>
2022-06-24 10:53:33 -0300

    feat(controller/post): created getPostById function

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -16,4 +16,15 @@ const getPosts = async (_req, res) => {
     res.status(200).json(posts);
 };
 
-module.exports = { createPost, getPosts };
+const getPostById = async (req, res) => {
+    const { id } = req.params;
+    const post = await postServices.getPostById(parseInt(id, 10));
+
+    if (!post) {
+        return res.status(404).json({ message: 'Post does not exist' });
+    }
+
+    res.status(200).json(post);
+};
+
+module.exports = { createPost, getPosts, getPostById };
